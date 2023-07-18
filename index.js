f4ea4170e1c0d25d6d23c7548912ffa23054330a
ivanapina <ivanapina@gmail.com>
2023-07-17 21:40:50 -0500

    Added Files to repo

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { generateSVG } = require("./library/generateSVG");
+const { generateShape } = require("./library/generateShape");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "logoName",
+      message: "Please enter 1-3 letters for your logo",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: `Please enter text color keyword or a hexadecimal number as the logo's text color`,
+    },
+    {
+      type: "input",
+      name: "logoColor",
+      message: `Please enter a color keyword or a hexadecimal number as the logo's background color`,
+    },
+    {
+      type: "list",
+      name: "logoShape",
+      message: `Please choose logo shape`,
+      choices: ["triangle", "circle", "square"],
+    },
+  ])
+  .then((data) => {
+    const svgPath = "./dist/logo.svg";
+    const finalLogo = generateShape(data);
+
+    //Generate the svg logo here.
+    fs.writeFile(svgPath, generateSVG(finalLogo), (err) =>
+      err ? console.error(err) : console.log("Generated logo.svg")
+    );
+  })
+  .catch((err) => console.error(err));
